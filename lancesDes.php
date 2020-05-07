88a5154bab887e4fea9e48e503b6b01b75c27056
benedicteb2054 <60343180+benedicteb2054@users.noreply.github.com>
2020-05-07 17:47:29 +0200

    Add files via upload

---
+++ lancesDes.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lances des</title>
+</head>
+<body>
+<!-- Je génére le lancé de Des  -->
+<form action="" method="post">
+        <label for="d2">D2</label>
+
+        <input type="number" id="d2" name="d2" min="0" max="10" value="0"><br>
+        <label for="d4">D4</label>
+
+        <input type="number" id="d4" name="d4" min="0" max="10" value="0"><br>
+        <label for="d6">D6</label>
+
+        <input type="number" id="d6" name="d6" min="0" max="10" value="0"><br>
+
+        <label for="d8">D8</label>
+
+        <input type="number" id="d8" name="d8" min="0" max="10" value="0"><br>
+</body>
+</html>
+
+
+<?php
+
